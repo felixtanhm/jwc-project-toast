1c266c6abd3863ea132f427d603f374868c10d8c
felixtanhm <felixtanhm@gmail.com>
2024-02-01 12:14:05 +0800

    Exercise 1 - Render various radio buttons - Make inputs controlled

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import Button from '../Button';
+import Button from "../Button";
 
-import styles from './ToastPlayground.module.css';
+import styles from "./ToastPlayground.module.css";
 
-const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
+const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 
 function ToastPlayground() {
+  const [message, setMessage] = React.useState("");
+  const [selectedVariant, setSelectedVariant] = React.useState("notice");
+
   return (
     <div className={styles.wrapper}>
       <header>
@@ -15,43 +18,52 @@ function ToastPlayground() {
       </header>
 
       <div className={styles.controlsWrapper}>
+        {/* Message Input */}
         <div className={styles.row}>
           <label
             htmlFor="message"
             className={styles.label}
-            style={{ alignSelf: 'baseline' }}
+            style={{ alignSelf: "baseline" }}
           >
             Message
           </label>
           <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} />
+            <textarea
+              id="message"
+              className={styles.messageInput}
+              onChange={(event) => {
+                setMessage(event.target.value);
+              }}
+              value={message}
+            />
           </div>
         </div>
-
+        {/* Variant Radio Buttons */}
         <div className={styles.row}>
           <div className={styles.label}>Variant</div>
-          <div
-            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
-            <label htmlFor="variant-notice">
-              <input
-                id="variant-notice"
-                type="radio"
-                name="variant"
-                value="notice"
-              />
-              notice
-            </label>
-
-            {/* TODO Other Variant radio buttons here */}
+          <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
+            {VARIANT_OPTIONS.map((variant) => {
+              const id = "variant-" + variant;
+              return (
+                <label key={id} htmlFor={id}>
+                  <input
+                    id={id}
+                    type="radio"
+                    name="variant"
+                    value={variant}
+                    checked={variant === selectedVariant}
+                    onChange={(event) => setSelectedVariant(event.target.value)}
+                  />
+                  {variant}
+                </label>
+              );
+            })}
           </div>
         </div>
-
+        {/* Submit Button */}
         <div className={styles.row}>
           <div className={styles.label} />
-          <div
-            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
+          <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
             <Button>Pop Toast!</Button>
           </div>
         </div>
