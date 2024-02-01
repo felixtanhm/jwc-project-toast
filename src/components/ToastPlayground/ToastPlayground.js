91d33f08bb8888b559c04a128f43a5636c1d8798
felixtanhm <felixtanhm@gmail.com>
2024-02-01 14:09:07 +0800

    Exercise 2 - Toast styling based on selection - Show and close single toast

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Button from "../Button";
+import Toast from "../Toast";
 
 import styles from "./ToastPlayground.module.css";
 
@@ -9,6 +10,11 @@ const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 function ToastPlayground() {
   const [message, setMessage] = React.useState("");
   const [selectedVariant, setSelectedVariant] = React.useState("notice");
+  const [isShown, setIsShown] = React.useState(false);
+
+  function toggleToast() {
+    setIsShown(!isShown);
+  }
 
   return (
     <div className={styles.wrapper}>
@@ -16,6 +22,13 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+      {isShown && (
+        <Toast
+          variant={selectedVariant}
+          message={message}
+          onClose={toggleToast}
+        />
+      )}
 
       <div className={styles.controlsWrapper}>
         {/* Message Input */}
@@ -64,7 +77,7 @@ function ToastPlayground() {
         <div className={styles.row}>
           <div className={styles.label} />
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button>Pop Toast!</Button>
+            <Button onClick={toggleToast}>Pop Toast!</Button>
           </div>
         </div>
       </div>
